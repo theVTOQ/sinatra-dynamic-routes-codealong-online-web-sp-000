504cbbef21ca1c86b00ade6d521a3e7e7f204321
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-24 21:18:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,9 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @num1 = params[:num1]
-    @num2 = params[:num2]
-    @num1 * @num2
+    #@num1 = params[:num1]
+    #@num2 = params[:num2]
+    #@num1 * @num2
+    erb :multiply
   end
 end
